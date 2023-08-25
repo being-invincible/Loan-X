4cf1c43d6bb6f76b4498d384b70f1d87741c8e34
Priya-tera <anelegistfrom98@yahoo.com>
2023-08-25 12:29:52 +0530

    @tanstack/react-table v1

--- app/tableview/page.js
+++ app/tableview/page.js
@@ -1,19 +1,19 @@
+"use client"
+
 import { useMemo } from 'react'
 // import {
 //     useReactTable,
 //     getCoreRowModel,
 //     flexRender
 // } from '@tanstack/react-table'
-const { ColumnDef, flexRender, getCoreRowModel, useReactTable } = require('@tanstack/react-table');
+const { flexRender, getCoreRowModel, useReactTable } = require('@tanstack/react-table');
 
 import fdata from './data.json'
 
-// const data = useMemo(() => fdata, [])
 
 // Data format
 /* 
 {
-      "fundDetails": 7,
       "number": 1,
       "user": "true",
       "customer": "N/A",
@@ -26,19 +26,83 @@ import fdata from './data.json'
 }
 */
 
-const columns = [
+
+
+const page = () => {
+
+  const data = useMemo(() => fdata, [])
+
+  const columns = [
     {
         header: 'ID',
         accesorKey: 'number',
         footer: 'ID',
-    }
-]
+    },
+    {
+      header: 'Customer',
+      accesorKey: 'customer',
+      footer: 'Customer',
+    },
+    {
+      header: 'User',
+      accesorKey: 'user',
+      footer: 'User',
+    },
+    {
+      header: 'Loan',
+      accesorKey: 'loan',
+      footer: 'Loan',
+    },
+    {
+      header: 'Amount',
+      accesorKey: 'amount',
+      footer: 'Amount',
+    },
+    {
+      header: 'Type',
+      accesorKey: 'type',
+      footer: 'Type',
+    },
+  ]
 
-const table = useReactTable({ data, columns })
+  const table = useReactTable({ data, columns, getCoreRowModel: getCoreRowModel() })
 
-const page = ({ data, columns }) => {
+
   return (
-    <div>page</div>
+    <div className='flex min-h-screen flex-col items-center justify-between p-10'>
+      <table>
+        <thead>
+        {table.getHeaderGroups().map(headerGroup => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map(header => (
+              <th key={header.id}>
+                {flexRender(
+                  header.column.columnDef.header, header.getContext()
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+        </thead>
+        
+        <tbody>
+          {table.getRowModel().rows.map(row =>(
+            <tr key={row.id}>
+              {row.getVisibleCells().map(cell => (
+                <td key={cell.id}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </td>
+              ))}
+            </tr>
+          ))}
+          
+        </tbody>
+
+        <tfoot>
+
+        </tfoot>
+      </table>
+    </div>
   )
 }
 
